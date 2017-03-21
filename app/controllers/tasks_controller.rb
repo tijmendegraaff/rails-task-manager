dc80d87ca3adb8ba91688a4ad1e7d81c04ae6923
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-21 16:58:08 +0100

    add styling and private method model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :load_task, only: [:show, :edit, :update, :destroy]
 
   def new
     @task = Task.new
@@ -12,11 +13,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     # @task.done = boolean_value_for_checkbox_inputs(task_params[:done])
     @task.update(task_params)
     redirect_to task_path
@@ -27,11 +26,9 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -42,6 +39,9 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :details, :done)
   end
 
+  def load_task
+    @task = Task.find(params[:id])
+  end
   # def boolean_value_for_checkbox_inputs(string)
   #   return true if ["1", 1, "true", true].include?(string)
   #   return false if ["0", 0, "false", false].include?(string)
